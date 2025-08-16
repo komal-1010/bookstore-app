b05a7c13028d12a4bc24c84744b377b746ed05ca
Komal Dhok <71183193+komal-1010@users.noreply.github.com>
2025-08-16 09:38:53 +0000

    fix: add Render domain to ALLOWED_HOSTS

--- backend/shop/settings.py
+++ backend/shop/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = "django-insecure-9wvcu40iyy7wmc@no%7p=4o&)aq32@(g&ay4cloaeu%!zw!li%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'bookstore-app-hsz2.onrender.com',  
+]
+
 
 
 # Application definition
