61fd0c4ac5216657321cf1f517539d0812a479db
Komal Dhok <71183193+komal-1010@users.noreply.github.com>
2025-08-18 15:56:08 +0000

    payment api changes

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -37,11 +37,6 @@ class CreateCheckoutSessionView(APIView):
 
         Payment.objects.create(
             order=order,
-            stripe_session_id=session.id,
-            status='pending'
-        )
-        Payment.objects.create(
-            order=order,
             user=request.user,
             stripe_session_id=session.id,
             status='pending'
