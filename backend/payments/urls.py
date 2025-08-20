6bf6199bce225f5d66b632cb6c60c572b2a49863
Komal Dhok <71183193+komal-1010@users.noreply.github.com>
2025-08-20 20:54:50 +0530

    Update urls.py

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -4,5 +4,5 @@ from .views import CreateCheckoutSessionView,stripe_webhook
 
 urlpatterns = [
     path('create-checkout-session/<int:order_id>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
-    path('webhooks/stripe/', stripe_webhook, name='stripe-webhook'),
+    path('webhooks/', stripe_webhook, name='stripe-webhook'),
 ]
